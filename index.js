65d906114ff06b4fd18881f0c4506400372564fe
lavinha754 <lavinhabianca4@gmail.com>
2026-08-04 20:13:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Alerta glicose João funcionando");
+const http = require("http");
+
+const glicose = "120";
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/glicose") {
+    res.writeHead(200, {
+      "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+      nome: "João",
+      glicose: glicose,
+      unidade: "mg/dL"
+    }));
+
+  } else {
+    res.writeHead(404);
+    res.end("Página não encontrada");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Servidor de glicose funcionando");
+});
